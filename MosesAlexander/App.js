41945218f33ffe9556f5c168dbb432fd0c039c10
Acipzz <moses.alexander@student.umn.ac.id>
2024-09-18 10:20:53 +0700

    Update App.js

--- MosesAlexander/App.js
+++ MosesAlexander/App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import data from './app.json';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Moses Alexander - 00000069818</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Data saya dan teman-teman saya</Text>
+      {data.length > 0 ? (
+        data.map((item, index) => (
+          <View key={index} style={styles.card}>
+            <Image source={{ uri: item.photo_url }} style={styles.image} />
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.email}>{item.email}</Text>
+          </View>
+        ))
+      ) : (
+        <Text style={styles.errorText}>Data tidak tersedia atau gagal dimuat.</Text>
+      )}
+    </ScrollView>
   );
 }
 
@@ -14,7 +24,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingVertical: 20,
+  },
+
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    marginTop: 10,
+  },
+
+  card: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 20,
+    padding: 10,
+    marginHorizontal: 20,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  email: {
+    fontSize: 18,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+
+  image: {
+    width: 220,
+    height: 220,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+
+  errorText: {
+    textAlign: 'center',
+    color: 'red',
   },
 });
