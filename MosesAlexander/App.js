e33256aaa17240eedfbbcbf9b6bee3774b608056
Acipzz <moses.alexander@student.umn.ac.id>
2024-09-18 10:45:28 +0700

    pindah style

--- MosesAlexander/App.js
+++ MosesAlexander/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import userData from './data.json';
+import styles from './App.styles.js';
 
 
 export default function App() {
@@ -28,40 +29,40 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    alignItems : "center",
-    justifyContent : "center",	
-    padding : 5,
-    display : "flex",
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: '#fff',
+//     alignItems : "center",
+//     justifyContent : "center",	
+//     padding : 5,
+//     display : "flex",
+//   },
 
-  card: {
-    borderWidth: 1,
-    borderColor: "black",
-    borderRadius: 8,
-    display : "flex",
-    flexDirection: "row",
-    alignItems : "center",
-    padding : 8,
-    width : 325,
-    gap : 8,
-  },
+//   card: {
+//     borderWidth: 1,
+//     borderColor: "black",
+//     borderRadius: 8,
+//     display : "flex",
+//     flexDirection: "row",
+//     alignItems : "center",
+//     padding : 8,
+//     width : 325,
+//     gap : 8,
+//   },
 
-  avatar: {
-    width: 75,
-    height: 75,
-    borderRadius: 999,
-  },
+//   avatar: {
+//     width: 75,
+//     height: 75,
+//     borderRadius: 999,
+//   },
 
-  boldText: {
-    fontWeight: "bold",
-  },
+//   boldText: {
+//     fontWeight: "bold",
+//   },
 
-  description: {
-    width: "fit-content",
-    display: "flex",
-    gap: 2,
-  },
-  });
+//   description: {
+//     width: "fit-content",
+//     display: "flex",
+//     gap: 2,
+//   },
+//   });
